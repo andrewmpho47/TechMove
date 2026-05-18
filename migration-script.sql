84db38cf873a5f07c8077001f67d678881333c0e
andrewmpho47 <andrewmpho47@gmail.com>
2026-05-18 20:18:53 +0200

    database miogration script

---
+++ migration-script.sql
@@ -0,0 +1,52 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+CREATE TABLE [Clients] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NOT NULL,
+    [ContactDetails] nvarchar(max) NOT NULL,
+    [Region] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Clients] PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [Contracts] (
+    [Id] int NOT NULL IDENTITY,
+    [StartDate] datetime2 NOT NULL,
+    [EndDate] datetime2 NOT NULL,
+    [Status] nvarchar(max) NOT NULL,
+    [ServiceLevel] nvarchar(max) NOT NULL,
+    [AgreementFilePath] nvarchar(max) NULL,
+    [ClientId] int NOT NULL,
+    CONSTRAINT [PK_Contracts] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Contracts_Clients_ClientId] FOREIGN KEY ([ClientId]) REFERENCES [Clients] ([Id]) ON DELETE CASCADE
+);
+
+CREATE TABLE [ServiceRequests] (
+    [Id] int NOT NULL IDENTITY,
+    [Description] nvarchar(max) NOT NULL,
+    [CostUSD] decimal(18,2) NOT NULL,
+    [CostZAR] decimal(18,2) NOT NULL,
+    [Status] nvarchar(max) NOT NULL,
+    [ContractId] int NOT NULL,
+    CONSTRAINT [PK_ServiceRequests] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_ServiceRequests_Contracts_ContractId] FOREIGN KEY ([ContractId]) REFERENCES [Contracts] ([Id]) ON DELETE CASCADE
+);
+
+CREATE INDEX [IX_Contracts_ClientId] ON [Contracts] ([ClientId]);
+
+CREATE INDEX [IX_ServiceRequests_ContractId] ON [ServiceRequests] ([ContractId]);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20260509182624_InitialCreate', N'10.0.7');
+
+COMMIT;
+GO
+
